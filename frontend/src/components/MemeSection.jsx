0c29881d2ae4b93778c7edb62b82eaec7c11adcb
Tashrique Ahmed <ta13@williams.edu>
2025-04-03 23:41:25 -0700

    backend url api changed

--- frontend/src/components/MemeSection.jsx
+++ frontend/src/components/MemeSection.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3001/api';
+const API_URL = 'https://vibecheck-project-2.onrender.com/api';
 
 const MemeSection = () => {
   const [memes, setMemes] = useState([]);
